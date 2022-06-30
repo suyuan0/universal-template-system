8c6a2dc682a11919b291bf223f5ad04c931c288c
培培 <614963845@qq.com>
2022-06-30 15:19:25 +0800

    feat(time): 定义时间戳

---
+++ src/utils/auth.js
@@ -0,0 +1,20 @@
+import { getItem, setItem } from '@/utils/storage'
+import { TIME_STAMP, TOKEN_TIMEOUT_VALUE } from '@/utils/constCon'
+// 获取时间戳
+export const getTimeStamp = () => {
+  return getItem(TOKEN_TIMEOUT_VALUE)
+}
+
+// 设置时间戳
+export const setTimeStamp = () => {
+  setItem(TIME_STAMP, Date.now())
+}
+
+// 判断是否超时
+export const isCheckTimeout = () => {
+  // 当前时间戳
+  const currentTime = Date.now()
+  // 缓存时间戳
+  const timeStamp = getTimeStamp()
+  return currentTime - timeStamp > TOKEN_TIMEOUT_VALUE
+}
--- src/utils/constCon.js
+++ src/utils/constCon.js
@@ -5,3 +5,7 @@
 export const TOKEN = 'token'
 
 export const USERINFO = 'userInfo'
+
+export const TIME_STAMP = 'timeStamp'
+
+export const TOKEN_TIMEOUT_VALUE = 2 * 36000 * 1000
