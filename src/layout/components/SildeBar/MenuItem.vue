5a2be1982332249b23a16eca82e34d244a5bbcf2
培培 <614963845@qq.com>
2022-06-30 20:09:16 +0800

    fix(menu): 修复menu菜单数据显示问题

--- src/layout/components/SildeBar/MenuItem.vue
+++ src/layout/components/SildeBar/MenuItem.vue
@@ -1,21 +1,21 @@
 <template>
   <template v-if='data.children && data.children.length>0'>
-    <el-sub-menu :index='data.id+""'>
+    <el-sub-menu :index='data.path'>
       <template #title>
         <el-icon>
-          <svg-icon :icon='data.icon' />
+          <svg-icon :icon='data.meta.icon' />
         </el-icon>
-        <span>{{ data.title }}</span>
+        <span>{{ data.meta.title }}</span>
       </template>
-      <menu-item v-for='item in data.children' :key='item.id' :data='item' />
+      <menu-item v-for='item in data.children' :key='item.path' :data='item' />
     </el-sub-menu>
   </template>
   <template v-if='data && !data.children'>
     <el-menu-item :index='data.path'>
       <el-icon>
-        <svg-icon :icon='data.icon' />
+        <svg-icon :icon='data.meta.icon' />
       </el-icon>
-      <span>{{ data.title }}</span>
+      <span>{{ data.meta.title }}</span>
     </el-menu-item>
   </template>
 </template>
