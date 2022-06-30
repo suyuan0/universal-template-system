5511392a8fa988a7bf3ab8822764a7d177ffdc75
培培 <614963845@qq.com>
2022-06-30 10:14:54 +0800

    feat(manage.vue): 导入导出按钮实现

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    员工管理
+  <div class='manage-container'>
+    <el-card class='header'>
+      <el-button type='primary'>excel 导入</el-button>
+      <el-button type='success'>excel 导出</el-button>
+    </el-card>
+    <el-card></el-card>
   </div>
 </template>
 
@@ -8,6 +12,12 @@
 
 </script>
 
-<style scoped>
-
+<style lang='scss' scoped>
+.manage-container {
+  .header {
+    height: 74px;
+    text-align: right;
+    margin-bottom: 22px;
+  }
+}
 </style>
